af9ac7856df97046de4e8b7eff46c67282dc8254
simocampi <37673011+simocampi@users.noreply.github.com>
2020-05-19 20:46:18 +0200

    esperimenti gui

--- frontend/GUI.py
+++ frontend/GUI.py
@@ -1,16 +1,28 @@
 import tkinter as tk
+import tkinter.ttk as ttk
 
 window_root = tk.Tk()
-
-window_root.geometry("800x600")
+window_root.style = ttk.Style()
+window_root.geometry("800x300")
 window_root.title('Voiced/Unvoiced-speech detection')
-window_root.resizable(False,False)
+window_root.style.theme_use('winnative')
 
-label = tk.Label(window_root, text="label1")
+print(window_root.style.theme_names())
+#style = ttk.Style()
+#style.theme_use('alt')
 
-button = tk.Button(text='Push to get your chash')
-button.grid(row=3 , column=2)
+
+
+
+
+#style.configure("BW.TLabel",foreground="black",background="white")
+
+#label = ttk.Label(window_root, text="label1", style="BW.TLabel" )
 #label.pack()
-
+#button = ttk.Button(text='Push to get your chash')
+#button.grid(row=3 , column=2)
+#label.pack()
+#style = tk.Style()
+#style.configure("BW.TLabel",foreground="black",background="white")
 
 window_root.mainloop()
