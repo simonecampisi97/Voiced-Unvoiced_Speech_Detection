80f61f18b92e05ea49efa9e3cfbbc7150eee1f33
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-05-24 10:05:08 +0200

    a

---
+++ ParametersExtraction.py
@@ -0,0 +1,20 @@
+import librosa
+import wave, struct
+from scipy.io import wavfile
+import soundfile as sf
+
+import numpy as np
+
+
+def ZRC():
+    pass
+
+def amplitude():
+    pass
+
+def energy():
+    pass
+
+def EMFCC():
+    pass
+
