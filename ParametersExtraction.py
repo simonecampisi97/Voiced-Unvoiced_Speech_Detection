a6f09ed529e3428ba93efb5faf3baf43acaacb08
simocampi <Sim0ne1997>
2020-05-24 16:53:12 +0200

    short-time zero-crossing rate

--- ParametersExtraction.py
+++ ParametersExtraction.py
@@ -1,5 +1,7 @@
+import librosa
 from librosa.feature import zero_crossing_rate
 from librosa.util import frame
+from librosa.util import example_audio_file
 import wave, struct
 from scipy.signal import get_window
 from scipy.io import wavfile
@@ -18,14 +20,19 @@ def split_audio():
 def st_zcr(y, fs, duration=30, overlap_rate=0.5, window='hann'):
     # Analysis frame length (in samples)
     frame_length = np.floor(duration * fs / 1000)
+    st_zcr_ = []
     shift = 1 - overlap_rate
     # shift length in samples -> hop_length
     frame_shift = round(frame_length * shift)
-
     # matrix where the rows contains contiguous slice
     frames = frame(y, frame_length=frame_length, hop_length=frame_shift, axis=0)
-    window = get_window(window=window)
-    return frames
+    window = get_window(window=window, Nx=frame_length, fftbins=False)
+    windowed_frame = np.multiply(frames, window)
+    for frame_w in windowed_frame:
+        st_zcr_.append(zero_crossing_rate(y=frame_w, frame_length=frame_length, hop_length=frame_shift))
+    return st_zcr_
+
+
 
 
 def amplitude():
