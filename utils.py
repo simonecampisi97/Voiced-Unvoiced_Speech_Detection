d6d7eeb2e36e78ed5e988867ca2349492939a767
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-05-26 10:28:41 +0200

    add high pass filter

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy import signal
 
 def nextpow2(x):
     return np.ceil(np.log2(abs(x)))
@@ -10,3 +10,15 @@ def frame_dft(frames, nfft):
     for frame in frames:
         S.append(np.fft.fft(frame, nfft))
     return S
+
+
+def butter_highpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = signal.butter(order, normal_cutoff, btype='high', analog=False)
+    return b, a
+
+def butter_highpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_highpass(cutoff, fs, order=order)
+    y = signal.filtfilt(b, a, data)
+    return y
