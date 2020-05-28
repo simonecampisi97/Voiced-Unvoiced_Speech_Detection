898ae5a00fd429465243ad83c354fdf12529bc70
simocampi <Sim0ne1997>
2020-05-28 12:24:53 +0200

    Gui icons

--- frontend/Widget/Button.py
+++ frontend/Widget/Button.py
@@ -3,6 +3,11 @@ import tkinter as tk
 
 class Button(tk.Button):
 
-    def __init__(self, root_window, x, y, text='', command=None, height=5, width=25):
-        super().__init__(text=text, command=command, master=root_window, width=width, height=height)
-        self.place(x=x, y=y)
+    def __init__(self, root_window, text=None, command=None, height=5, width=5, path_img=None, image=None):
+        super().__init__(text=text, command=command, master=root_window,
+                         width=width, height=height, borderwidth=0, image=image)
+
+        if path_img is not None:
+            img = tk.PhotoImage(file=path_img)
+            img.subsample(5,5)
+            self.config(image=img)
--- frontend/Widget/Frame_.py
+++ frontend/Widget/Frame_.py
@@ -3,6 +3,8 @@ import tkinter as tk
 
 class Frame(tk.Frame):
 
-    def __init__(self, window_root, text, x, y, ):
-        super().__init__(text=text, master=window_root)
+    def __init__(self, window_root, x, y, height, width, borderwidth, relief, command=None):
+        super().__init__(master=window_root, height=height, width=width, relief=relief,
+                         borderwidth=borderwidth, command=command, bd=1)
         self.place(x=x, y=y)
+        self.pack_propagate(False)
