73a19e5138568a66692a8b193d82ba048d9b5db6
simocampi <37673011+simocampi@users.noreply.github.com>
2020-05-28 19:40:42 +0200

    plot_parameter, home button

--- frontend/Widget/Frame_.py
+++ frontend/Widget/Frame_.py
@@ -5,6 +5,6 @@ class Frame(tk.Frame):
 
     def __init__(self, window_root, x, y, height, width, borderwidth, relief, command=None):
         super().__init__(master=window_root, height=height, width=width, relief=relief,
-                         borderwidth=borderwidth, command=command, bd=1)
+                         borderwidth=borderwidth, command=command, bd=0)
         self.place(x=x, y=y)
         self.pack_propagate(False)
--- frontend/Widget/Window.py
+++ frontend/Widget/Window.py
@@ -1,40 +1,75 @@
 import tkinter as tk
 from tkinter import ttk
 from frontend.Widget import Label, Button, Frame_
-from PIL import Image, ImageTk
+
+BACK_GROUND_COLOR = '#D6DBDF'
+WIDTH_WINDOW = 800
+HEIGHT_WINDOW = 500
 
 
 class Window(tk.Tk):
 
-    def __init__(self, title, geometry, resizable):
+    def __init__(self, title, resizable):
         super().__init__()
+        # -----------ROOT WINDOWS-------------
         self.title(title)
         self.hidden = 0
         self.resizable(resizable, resizable)
-        self.geometry(geometry)
-        self.toolbar = None
-        self.button_tool1 = None
-        self.button_tool1_img = tk.PhotoImage(file='frontend/Widget/icons/train.png')#.subsample(x=45, y=45)
+        self.geometry(str(WIDTH_WINDOW) + 'x' + str(HEIGHT_WINDOW))
+        self.configure(bg=BACK_GROUND_COLOR)
 
+        # ------------TOOLBAR------------------
+        self.toolbar = None
+        self.button_graphic = None
+        self.button_graphics_img = tk.PhotoImage(file='frontend/Widget/icons/graphic.png')
+        self.label_graphic = None
         self.button_tool2 = None
 
-        self.menu_img = tk.PhotoImage(file='frontend/Widget/icons/menu.png')
-        self.menu_button = Button.Button(root_window=self, image=self.menu_img, height=64, width=64)
-        self.menu_button.bind('<Button-1>', self.show_frame_menu)
-        self.menu_button.place(x=2, y=2)
+        # ----------Menu Bar-------------------------
 
+        self.menu_frame = Frame_.Frame(window_root=self, height=HEIGHT_WINDOW, width=50, x=65, y=0,
+                                       borderwidth=5, relief='groove')
+
+        self.menu_frame.configure(highlightbackground="black",
+                                  highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR)
+        self.menu_frame.place(x=0)
+
+        # Menu Button
+        self.menu_img = tk.PhotoImage(file='frontend/Widget/icons/menu.png')
+        self.menu_button = Button.Button(root_window=self.menu_frame, image=self.menu_img, height=25, width=25)
+        self.menu_button.configure(bg=BACK_GROUND_COLOR)
+        self.menu_button.bind('<Button-1>', self.show_frame_menu)
+        self.menu_button.place(x=10, y=10)
+
+        # Back Button
         self.back_img = tk.PhotoImage(file='frontend/Widget/icons/back.png')
-        self.back_button = Button.Button(root_window=self, image=self.back_img, height=64, width=64)
+        self.back_button = Button.Button(root_window=self.menu_frame, image=self.back_img, height=25, width=25)
+        self.back_button.configure(bg=BACK_GROUND_COLOR)
         self.back_button.bind('<Destroy>', self.destroy_)
-        self.back_button.place(x=2, y=66)
+        self.back_button.place(x=10, y=46)
+
+        # Home Button
+        self.home_img = tk.PhotoImage(file='frontend/Widget/icons/home.png')
+        self.home_button = Button.Button(root_window=self.menu_frame, image=self.home_img, height=25, width=25)
+        self.home_button.configure(bg=BACK_GROUND_COLOR)
+        self.home_button.place(x=10, y=82)
 
     def show_frame_menu(self, event):
-        self.toolbar = Frame_.Frame(window_root=self, height=65, width=801, x=65, y=0,
-                                    borderwidth=3, relief='groove')
+        self.toolbar = Frame_.Frame(window_root=self, height=60, width=WIDTH_WINDOW - 54, x=51, y=0,
+                                    borderwidth=5, relief='groove')
+        self.toolbar.configure(highlightbackground="black", highlightcolor="black",
+                               highlightthickness=1, bg=BACK_GROUND_COLOR)
 
-        self.button_tool1 = Button.Button(root_window=self.toolbar, text='Test/Train', image=self.button_tool1_img,
-                                          height=67, width=67)
-        self.button_tool1.place(x=50, y=2)
+        self.button_graphic = Button.Button(root_window=self.toolbar, text='Test/Train',
+                                            image=self.button_graphics_img, height=25, width=25)
+        self.button_graphic.configure(bg=BACK_GROUND_COLOR)
+        self.button_graphic.place(x=55, y=5)
+        self.label_graphic = Label.Label(window_root=self.toolbar, text='Plot Parameters',
+                                         x=15, y=30, height=0, width=15)
+        self.label_graphic.configure(bg=BACK_GROUND_COLOR)
+
+    def show_frame_graphics(self):
+        pass
 
     def destroy_(self, event):
         self.toolbar.destroy()
