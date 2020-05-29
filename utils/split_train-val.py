8e35d41d3e818db5b6b5520b0449dbea2d010696
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-05-29 18:28:27 +0200

    final version script split_train-val.py

--- utils/split_train-val.py
+++ utils/split_train-val.py
@@ -13,7 +13,7 @@ def create_tree_directory(root, t="", val_size=0.33, data_dirs=[]):
         if os.path.isdir(os.path.join(root, t, folder)):
             data_dirs = create_tree_directory(root, os.path.join(t, folder), val_size=val_size, data_dirs=data_dirs)
         else:
-            data_dirs.append(os.path.join(root, t))
+            data_dirs.append(t)
             break
 
     return data_dirs
@@ -21,7 +21,9 @@ def create_tree_directory(root, t="", val_size=0.33, data_dirs=[]):
 
 def split_data(root, data_folder, splitting_val):
     train_files = os.listdir(os.path.join(root, data_folder))
-    total_size = np.sum([os.path.getsize(file) for file in os.listdir(os.path.join(root, data_folders))])
+
+    total_size = np.sum([os.path.getsize(os.path.join(root, data_folder, file))
+                         for file in os.listdir(os.path.join(root, data_folder))])
 
     id_val_files = []
     curr_val_size = 0
@@ -30,7 +32,7 @@ def split_data(root, data_folder, splitting_val):
     while True:
         idx = random.randrange(len(train_files))
         idx_size = os.path.getsize(os.path.join(root, data_folder, train_files[idx]))
-        if val_size > curr_val_size + idx_size:
+        if curr_val_size + idx_size > val_size:
             break
         del train_files[idx]
         curr_val_size = curr_val_size + idx_size
@@ -41,7 +43,10 @@ def split_data(root, data_folder, splitting_val):
 
 def move_data(root, list_of_folder, split_val=0.33):
     for folders in list_of_folder:
+        print("\tCoping file for speaker", folders[0].split("\\")[-1], "...")
         pos_val_files = split_data(root, folders[1], split_val)
+        print("\t\t-training set: {} files\n\t\t-validation set: {} files".format(234 - len(pos_val_files),
+                                                                                  len(pos_val_files)))
 
         for data_folder in folders:
             train_files = os.listdir(os.path.join(root, data_folder))
@@ -68,11 +73,15 @@ if __name__ == "__main__":
     print("creating tree directory for training and validation...")
     data_folders = create_tree_directory(data_dir, val_size=split)
 
-    test = []
-    for folder in data_folders:
-        print(folder)
+    group_of_folder = []
+
+    while data_folders:
+        folder = data_folders[0]
+
         speaker = folder.split("\\")[-1]
         ls = [i for i in data_folders if speaker in i]
         for s in ls:
             data_folders.remove(s)
-        test.append(ls)
+        group_of_folder.append(ls)
+
+    move_data(data_dir, group_of_folder, split)
