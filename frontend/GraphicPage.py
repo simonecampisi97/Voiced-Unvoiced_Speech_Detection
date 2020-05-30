3cbf4257f2d91d8e09cc77fcb1c0420fb0adcd13
simocampi <Sim0ne1997>
2020-05-30 12:29:09 +0200

    Add Train/Test buttun to Menu Page

--- frontend/GraphicPage.py
+++ frontend/GraphicPage.py
@@ -1,35 +1,40 @@
 import tkinter as tk
 from frontend.Settings import *
+from frontend.HomePage import *
 
 
 class GraphicPage(tk.Frame):
-    def __init__(self, parent, controller):
+    def __init__(self, parent, controller, home_page):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+        self.home_page = home_page
+        self.bg = BACK_GROUND_COLOR
 
-        self.graphic_frame = tk.Frame(master=self, height=HEIGHT_WINDOW - 54,
-                                      width=WIDTH_WINDOW - 54, borderwidth=5, relief='groove',
-                                      highlightbackground="black", highlightcolor="black", highlightthickness=1,
-                                      bg=BACK_GROUND_COLOR).place(x=51, y=60)
+        label = tk.Label(self, text="GRAPHIC PAGE", font=controller.title_font, bg=BACK_GROUND_COLOR)
+        label.pack(side="top", fill="x", pady=10)
 
-        self.menu_img = tk.PhotoImage(file='frontend/Widget/icons/menu.png')
+        # ------------------------------MENU BAR-------------------------------------------------------
+
+        #home_page.back_button.bind('<Button-1>', lambda: controller.show_frame("MenuPage"))
+
+        self.menu_frame = tk.Frame(master=self, height=HEIGHT_WINDOW, width=50,
+                                   borderwidth=2, relief='flat', highlightbackground="black",
+                                   highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place(x=0, y=0)
+
+        self.menu_img = tk.PhotoImage(file='frontend/icons/menu.png')
         self.menu_button = tk.Button(master=self, image=self.menu_img,
-                                     height=25, width=25,
+                                     height=25, width=25, relief='flat',
                                      command=lambda: controller.show_frame("MenuPage"),
                                      bg=BACK_GROUND_COLOR).place(x=10, y=10)
 
-        self.menu_frame = tk.Frame(master=self, height=HEIGHT_WINDOW, width=50,
-                                   borderwidth=5, relief='groove', highlightbackground="black",
-                                   highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place(x=0, y=0)
-
         # Back Button
-        self.back_img = tk.PhotoImage(file='frontend/Widget/icons/back.png')
+        self.back_img = tk.PhotoImage(file='frontend/icons/back.png')
         self.back_button = tk.Button(master=self.menu_frame, image=self.back_img, height=25, width=25,
-                                     bg=BACK_GROUND_COLOR,
+                                     bg=BACK_GROUND_COLOR, relief='flat',
                                      command=lambda: controller.show_frame("MenuPage")).place(x=10, y=46)
 
         # Home Button
-        self.home_img = tk.PhotoImage(file='frontend/Widget/icons/home.png')
+        self.home_img = tk.PhotoImage(file='frontend/icons/home.png')
         self.home_button = tk.Button(master=self.menu_frame, image=self.home_img, height=25, width=25,
-                                     bg=BACK_GROUND_COLOR,
+                                     bg=BACK_GROUND_COLOR, relief='flat',
                                      command=lambda: controller.show_frame("HomePage")).place(x=10, y=82)
