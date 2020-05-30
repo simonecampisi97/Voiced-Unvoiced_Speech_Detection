207bf7d88683964791c8bc3ba6788c6605a1718e
simocampi <Sim0ne1997>
2020-05-30 10:39:35 +0200

    fixed back and home button

--- frontend/GraphicPage.py
+++ frontend/GraphicPage.py
@@ -8,5 +8,28 @@ class GraphicPage(tk.Frame):
         self.controller = controller
 
         self.graphic_frame = tk.Frame(master=self, height=HEIGHT_WINDOW - 54,
-                                      width=WIDTH_WINDOW - 54, borderwidth=5, relief='groove', highlightbackground="black",
-                                      highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place( x=51,y=60)
+                                      width=WIDTH_WINDOW - 54, borderwidth=5, relief='groove',
+                                      highlightbackground="black", highlightcolor="black", highlightthickness=1,
+                                      bg=BACK_GROUND_COLOR).place(x=51, y=60)
+
+        self.menu_img = tk.PhotoImage(file='frontend/Widget/icons/menu.png')
+        self.menu_button = tk.Button(master=self, image=self.menu_img,
+                                     height=25, width=25,
+                                     command=lambda: controller.show_frame("MenuPage"),
+                                     bg=BACK_GROUND_COLOR).place(x=10, y=10)
+
+        self.menu_frame = tk.Frame(master=self, height=HEIGHT_WINDOW, width=50,
+                                   borderwidth=5, relief='groove', highlightbackground="black",
+                                   highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place(x=0, y=0)
+
+        # Back Button
+        self.back_img = tk.PhotoImage(file='frontend/Widget/icons/back.png')
+        self.back_button = tk.Button(master=self.menu_frame, image=self.back_img, height=25, width=25,
+                                     bg=BACK_GROUND_COLOR,
+                                     command=lambda: controller.show_frame("MenuPage")).place(x=10, y=46)
+
+        # Home Button
+        self.home_img = tk.PhotoImage(file='frontend/Widget/icons/home.png')
+        self.home_button = tk.Button(master=self.menu_frame, image=self.home_img, height=25, width=25,
+                                     bg=BACK_GROUND_COLOR,
+                                     command=lambda: controller.show_frame("HomePage")).place(x=10, y=82)
--- frontend/MenuPage.py
+++ frontend/MenuPage.py
@@ -23,3 +23,27 @@ class MenuPage(tk.Frame):
 
         self.label_graphic = tk.Label(master=self, text='Plot Parameters',
                                       height=0, width=12, bg=BACK_GROUND_COLOR).place(x=55, y=40)
+
+        # Menu Button
+
+        self.menu_frame = tk.Frame(master=self, height=HEIGHT_WINDOW, width=50,
+                                   borderwidth=5, relief='groove', highlightbackground="black",
+                                   highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place(x=0, y=0)
+
+        self.menu_img = tk.PhotoImage(file='frontend/Widget/icons/menu.png')
+        self.menu_button = tk.Button(master=self.menu_frame, image=self.menu_img,
+                                     height=25, width=25,
+                                     command=lambda: controller.show_frame("MenuPage"),
+                                     bg=BACK_GROUND_COLOR).place(x=10, y=10)
+
+        # Back Button
+        self.back_img = tk.PhotoImage(file='frontend/Widget/icons/back.png')
+        self.back_button = tk.Button(master=self.menu_frame, image=self.back_img, height=25, width=25,
+                                     bg=BACK_GROUND_COLOR,
+                                     command=lambda: controller.show_frame("HomePage")).place(x=10, y=46)
+
+        # Home Button
+        self.home_img = tk.PhotoImage(file='frontend/Widget/icons/home.png')
+        self.home_button = tk.Button(master=self.menu_frame, image=self.home_img, height=25, width=25,
+                                     bg=BACK_GROUND_COLOR,
+                                     command=lambda: controller.show_frame("HomePage")).place(x=10, y=82)
