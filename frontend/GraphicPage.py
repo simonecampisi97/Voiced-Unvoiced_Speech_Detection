c6a0c63d303f9ff99a1464c90f25dc37b41dc3b8
simocampi <37673011+simocampi@users.noreply.github.com>
2020-05-30 21:30:33 +0200

    Tolta funzionalità back button e spostato la sidebar in GUI

--- frontend/GraphicPage.py
+++ frontend/GraphicPage.py
@@ -12,25 +12,6 @@ class GraphicPage(tk.Frame):
         self.bg = BACK_GROUND_COLOR
         self.font_label = self.title_font = tkfont.Font(family='Helvetica', size=10, weight="bold", slant="italic")
 
-        # ------------------------------MENU BAR-------------------------------------------------------
-
-        self.menu_frame = tk.LabelFrame(master=self, height=HEIGHT_WINDOW, width=50,
-                                        borderwidth=2, relief='flat', highlightbackground="black",
-                                        highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place(x=0,
-                                                                                                                  y=0)
-
-        self.menu_img = tk.PhotoImage(file='frontend/icons/menu.png')
-        self.menu_button = tk.Button(master=self.menu_frame, image=self.menu_img,
-                                     height=25, width=25, relief='flat',
-                                     command=lambda: controller.show_frame("MenuPage"),
-                                     bg=BACK_GROUND_COLOR).place(x=10, y=10)
-
-        # Back Button
-        self.back_img = tk.PhotoImage(file='frontend/icons/back.png')
-        self.back_button = tk.Button(master=self.menu_frame, image=self.back_img, height=25, width=25,
-                                     bg=BACK_GROUND_COLOR, relief='flat',
-                                     command=lambda: controller.show_frame("MenuPage")).place(x=10, y=46)
-
         # --------------------------------------------------------------------------------------------------
 
         self.upload_frame = tk.LabelFrame(master=self, text='Upload File Audio', font=self.font_label, height=100,
--- frontend/MenuPage.py
+++ frontend/MenuPage.py
@@ -4,7 +4,7 @@ from frontend.HomePage import HomePage
 from tkinter import font as tkfont
 
 
-class MenuPage(HomePage):
+class MenuPage(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
@@ -12,17 +12,6 @@ class MenuPage(HomePage):
         self.bg = BACK_GROUND_COLOR
 
         self.font_label = self.title_font = tkfont.Font(family='Helvetica', size=10, weight="bold", slant="italic")
-
-        # -----------------MENU BAR---------------------
-        self.menu_frame = tk.Frame(master=self, height=HEIGHT_WINDOW, width=50,
-                                   borderwidth=2, relief='flat', highlightbackground="black",
-                                   highlightcolor="black", highlightthickness=1, bg=BACK_GROUND_COLOR).place(x=0, y=0)
-
-        # Back Button
-        self.back_img = tk.PhotoImage(file='frontend/icons/back.png')
-        self.back_button = tk.Button(master=self.menu_frame, image=self.back_img, height=25, width=25,
-                                     bg=BACK_GROUND_COLOR, relief='flat',
-                                     command=lambda: controller.show_frame("HomePage")).place(x=10, y=46)
 
         # --------------------------------------------------toolbar--------------------------------------
 
@@ -34,7 +23,7 @@ class MenuPage(HomePage):
         self.button_graphics_img = tk.PhotoImage(file='frontend/icons/graphic.png')
         self.button_graphic = tk.Button(master=self, relief='flat',
                                         image=self.button_graphics_img, height=25, width=25, bg=BACK_GROUND_COLOR,
-                                        command=lambda: controller.show_frame("GraphicPage")).place(x=150, y=5)
+                                        command=lambda: self.controller.show_frame("GraphicPage")).place(x=150, y=5)
 
         self.label_graphic = tk.Label(master=self, text='Plot',
                                       height=0, width=12, bg=BACK_GROUND_COLOR, font=self.font_label).place(x=115, y=38)
@@ -42,7 +31,12 @@ class MenuPage(HomePage):
         self.button_train_img = tk.PhotoImage(file='frontend/icons/train.png')
         self.button_train = tk.Button(master=self, text='Test/Train', relief='flat',
                                       image=self.button_train_img, height=25, width=25, bg=BACK_GROUND_COLOR,
-                                      command=lambda: controller.show_frame("TrainTest")).place(x=250, y=5)
+                                      command=lambda: self.controller.show_frame("TrainTest")).place(x=250, y=5)
 
         self.label_train = tk.Label(master=self, text='Train/Test',
-                                    height=0, width=12, bg=BACK_GROUND_COLOR, font=self.font_label).place(x=215, y=38)
+                                    height=0, width=12, bg=BACK_GROUND_COLOR,
+                                    font=self.font_label).place(x=215, y=38)
+
+    def go_home(self):
+        self.toolbar_frame_.destroy()
+        self.controller.show_frame('HomePage')
