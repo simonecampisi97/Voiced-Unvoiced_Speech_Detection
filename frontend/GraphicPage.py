d692bb31d3f713dde00f53f3b549f802addaa0c7
simocampi <Sim0ne1997>
2020-05-31 16:56:22 +0200

    zcr graphic - tool bar graphic

--- frontend/GraphicPage.py
+++ frontend/GraphicPage.py
@@ -8,9 +8,12 @@ import matplotlib.pyplot as plt
 import matplotlib
 from scipy.io import wavfile
 import numpy as np
+from Frames import Frames
+from utils.utils import *
+from ParametersExtraction import *
 
 matplotlib.use("TkAgg")
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 
 def popup_message(msg):
@@ -23,6 +26,26 @@ def popup_message(msg):
     label.pack(pady=20)
     B1 = ttk.Button(popup, text="exit", command=popup.destroy)
     B1.pack(side='bottom', pady=25)
+
+
+def plot_on_tab(figure, master):
+    canvas = FigureCanvasTkAgg(figure=figure, master=master)
+    canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+    toolbar = NavigationToolbar2Tk(canvas, master)
+    toolbar.update()
+
+
+def create_frame_plot(tab):
+    frame = tk.Frame(master=tab, height=HEIGHT_WINDOW - 55,
+                     width=WIDTH_WINDOW - 215)
+    frame.place(x=0, y=0)
+    return frame
+
+
+def update_frame_plot(frame, tab):
+    frame.destroy()
+    frame_ = create_frame_plot(tab=tab)
+    return frame_
 
 
 class GraphicPage(tk.Frame):
@@ -49,14 +72,17 @@ class GraphicPage(tk.Frame):
                                        width=WIDTH_WINDOW - 210)
 
         self.tab_VUV = ttk.Frame(self.tabControl)
-        self.frame_plot = self.create_frame_plot()
+        self.frame_plot = create_frame_plot(tab=self.tab_VUV)
 
-        self.tab_plot2 = ttk.Frame(self.tabControl)
-        self.tab_plot3 = ttk.Frame(self.tabControl)
+        self.tab_zcr = ttk.Frame(self.tabControl)
+        self.frame_plot2 = create_frame_plot(tab=self.tab_zcr)
+
+        self.tab_mag = ttk.Frame(self.tabControl)
+
         self.tabControl.place(x=200, y=20)
         self.tabControl.add(self.tab_VUV, text='VUV')
-        self.tabControl.add(self.tab_plot2, text='plot2')
-        self.tabControl.add(self.tab_plot3, text='plot3')
+        self.tabControl.add(self.tab_zcr, text='st-zcr')
+        self.tabControl.add(self.tab_mag, text='st-magnitude')
 
         self.plot_button = tk.Button(master=self, text='Plot',
                                      height=1, width=10, relief='groove',
@@ -71,27 +97,27 @@ class GraphicPage(tk.Frame):
                                 textvariable=var, font=font).place(x=70, y=75)
         var.set(str(self.file_path).split('/')[-1])
 
-    def create_frame_plot(self):
-        frame = tk.Frame(master=self.tab_VUV, height=HEIGHT_WINDOW - 55,
-                         width=WIDTH_WINDOW - 215)
-        frame.place(x=0, y=0)
-        return frame
-
-    def update_frame_plot(self, frame):
-        frame.destroy()
-        frame_ = self.create_frame_plot()
-        return frame_
-
     def plot_signal(self):
-        self.frame_plot = self.update_frame_plot(self.frame_plot)
+        self.frame_plot = update_frame_plot(self.frame_plot, tab=self.tab_VUV)
+        self.frame_plot2 = update_frame_plot(self.frame_plot2, tab=self.tab_zcr)
+
         if self.file_path == "":
             popup_message("Upload The file first!")
             return
-        fs, y = wavfile.read(self.file_path)
 
+        # read wav file
+        fs, y = wavfile.read(self.file_path)
+        frames = Frames(fs=fs, y=y)
+        # time axis (milliseconds)
         time = np.arange(len(y)) * 1000 * (1 / fs)
-        figure = plt.Figure(figsize=(6.5, 5), dpi=110)
+
+        figure = plt.Figure(figsize=(9, 5), dpi=90)
         ax = figure.add_subplot(111)
-        ax.plot(time, y)
-        chart_type = FigureCanvasTkAgg(figure=figure, master=self.frame_plot)
-        chart_type.get_tk_widget().pack()
+        ax.plot(time, y, ms=1)
+        plot_on_tab(figure=figure, master=self.frame_plot)
+
+        figure2 = plt.Figure(figsize=(9, 5), dpi=90)
+        ax2 = figure2.add_subplot(111)
+        zcr = st_zcr(frames)
+        plot_result(signal=zcr, Frames=frames, ax=ax2)
+        plot_on_tab(figure=figure2, master=self.frame_plot2)
