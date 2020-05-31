417e6f8e1dbb05766e13ed870341e9035bf3ad73
simocampi <Sim0ne1997>
2020-05-31 13:53:42 +0200

    BackButton quasi funzionante

--- frontend/HomePage.py
+++ frontend/HomePage.py
@@ -7,7 +7,7 @@ class HomePage(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.bg = BACK_GROUND_COLOR
-
+        #self.controller.back_button.configure(command=lambda: self.controller.show_frame('HomePage'))
         label = tk.Label(self, text="HOME PAGE", font=self.controller.title_font, bg=BACK_GROUND_COLOR)
         label.pack(side="top", fill="x", pady=10)
 
--- frontend/TrainTestPage.py
+++ frontend/TrainTestPage.py
@@ -8,6 +8,7 @@ class TrainTest(tk.Frame):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.bg = BACK_GROUND_COLOR
+        self.controller.back_button.configure(command=lambda: self.controller.show_frame('MenuPage'))
 
         label = tk.Label(self, text="Train/Test PAGE", font=controller.title_font, bg=BACK_GROUND_COLOR)
         label.pack(side="top", fill="x", pady=10)
