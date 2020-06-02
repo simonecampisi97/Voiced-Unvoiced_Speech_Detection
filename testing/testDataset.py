2f11a1dbcf271c339d85208473a6d5204d29e1ad
simocampi <Sim0ne1997>
2020-06-02 13:32:40 +0200

    Saved Dataset

--- testing/testDataset.py
+++ testing/testDataset.py
@@ -1,6 +1,7 @@
 import torchvision.transforms as transforms
 from librosa.core import load
 from sklearn.model_selection import train_test_split
+from utils.saveVariable import *
 
 from DataLoader import DataLoader
 from DataSet import DataSet
@@ -10,26 +11,20 @@ if __name__ == "__main__":
     dataset_dir = "C:\\Users\\simoc\\Documents\\SPEECH_DATA_ZIPPED\\SPEECH DATA"
     # dataset_dir = "C:\\Users\\carot\\Documents\\SPEECH_DATA_ZIPPED\\SPEECH DATA"
 
-    transform = transforms.Compose(
-        [transforms.ToTensor()])
+    try:
+        ds = load_var("Model/dataset")
+        print('Load dataset from binary file...')
+    except FileNotFoundError:
+        print('creating dataset')
+        dl = DataLoader(dataset_dir)
+        ds = DataSet(dl)
+        save_var(ds, "Model/dataset")
 
-    dl = DataLoader(dataset_dir)
-
-    ds = DataSet(dl)
     ds.info()
+
     print('feature. ', ds.features.shape)
-    _, feature, label = dl[0]
+
     X_train, X_test, y_train, y_test = train_test_split(ds.features, ds.labels, test_size=0.33, random_state=42)
 
     print('Train: ', X_train.shape)
     print('Test: ', X_test.shape)
-
-    print("Type feature:", type(feature))
-    print("Type labels:", (type(label)))
-    print()
-
-    print("Size feature:", feature.shape)
-    print("Size labels:", label.shape)
-    print()
-
-    print(type(label[0]), label[0])
