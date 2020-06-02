4e7d825730210f839cff31debef844c5081f211f
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-06-02 16:29:49 +0200

    code refactoring and removed unused file

--- testing/testDataset.py
+++ testing/testDataset.py
@@ -1,30 +1,34 @@
-import torchvision.transforms as transforms
-from librosa.core import load
+import time
+
 from sklearn.model_selection import train_test_split
-from utils.saveVariable import *
 
 from DataLoader import DataLoader
 from DataSet import DataSet
-from ParametersExtraction import *
+from utils.saveVariable import *
 
 if __name__ == "__main__":
-    dataset_dir = "C:\\Users\\simoc\\Documents\\SPEECH_DATA_ZIPPED\\SPEECH DATA"
-    # dataset_dir = "C:\\Users\\carot\\Documents\\SPEECH_DATA_ZIPPED\\SPEECH DATA"
+    # dataset_dir = "C:\\Users\\simoc\\Documents\\SPEECH_DATA_ZIPPED\\SPEECH DATA"
+    dataset_dir = "C:\\Users\\carot\\Documents\\SPEECH_DATA_ZIPPED\\SPEECH DATA"
 
     try:
-        ds = load_var("Model/dataset")
-        print('Load dataset from binary file...')
+        ds = load_var("./dataset.save")
+        print('Dataset loaded from a local file')
     except FileNotFoundError:
-        print('creating dataset')
+        print('Creating the dataset:', flush=True)
         dl = DataLoader(dataset_dir)
+        print("\t-Found {} files".format(len(dl)))
+
+        print("\t-Extracting features from row data...", flush=True)
+        time.sleep(0.01)
         ds = DataSet(dl)
-        save_var(ds, "Model/dataset")
+        save_var(ds, "./dataset.save")
+        print("\t-Dataset saved as local file")
 
+    print()
     ds.info()
-
-    print('feature. ', ds.features.shape)
 
     X_train, X_test, y_train, y_test = train_test_split(ds.features, ds.labels, test_size=0.33, random_state=42)
 
+    print()
     print('Train: ', X_train.shape)
     print('Test: ', X_test.shape)
