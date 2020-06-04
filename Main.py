e820c9b5f7aab042fe7a9421e5d7c0e7c21fd82e
simocampi <Sim0ne1997>
2020-06-04 17:12:48 +0200

    button evaluate, e altre cose

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 
 if __name__ == '__main__':
 
-    config = tf.ConfigProto()
+    config = tf.compat.v1.ConfigProto()
     config.gpu_options.allow_growth = True
-    session = tf.Session(config=config)
+    session = tf.compat.v1.Session(config=config)
 
     app = App()
     app.mainloop()
--- frontend/GUI.py
+++ frontend/GUI.py
@@ -18,6 +18,7 @@ class App(tk.Tk):
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
         self.iconbitmap(default='frontend/icons/speech1.ico')
         # self.iconwindow(pathName=tk.PhotoImage('frontend/icons/speech1.ico'))
+
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
         # will be raised above the others
