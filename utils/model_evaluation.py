0557ecd512c4bf3d713e65ac77afcfecc99609e4
Alessandro <AlessandroCaroti@users.noreply.github.com>
2020-06-04 11:22:05 +0200

    add evaluation function

---
+++ utils/model_evaluation.py
@@ -0,0 +1,29 @@
+import time
+
+from tensorflow.keras import Sequential
+
+from DataLoader import DataLoader
+from DataSet import DataSet
+
+
+def load_evaluation_data(evaluate_data_dir: str):
+    dl = DataLoader(evaluate_data_dir)
+
+    time.sleep(0.01)
+    ds = DataSet(dl)
+
+    file_num = len(dl)
+    frame_num = len(ds)
+
+    return ds, file_num, frame_num
+
+
+def evaluate_model(model: Sequential, testSet: DataSet, verbose=2):
+    results = model.evaluate(x=testSet.features, y=testSet.labels, batch_size=128, verbose=verbose)
+
+    accuracy = results[-1]
+    loss = results[0]
+
+    print(model.metrics)
+
+    return accuracy, loss
